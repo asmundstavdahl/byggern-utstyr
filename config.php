9e6d4912580fdc0a6a78f6453aff001fd706e325
Åsmund Stavdahl <asmund.stavdahl@ntnu.no>
2019-07-03 15:32:25 +0200

    fix the config.php fix

--- config.php
+++ config.php
@@ -25,9 +25,9 @@ Database::$pdo = new PDO("sqlite:{$dbFile}");
 use \Rapd\Environment;
 
 # For JS, CSS, images etc.: (ASSET_BASE)/css/app.css
-Environment::set("ASSET_BASE", "");
+Environment::set("ASSET_BASE", "/byggern-utstyr");
 
-Environment::set("TITLE", "/byggern-utstyr");
+Environment::set("TITLE", "byggern-utstyr");
 Environment::set("AUTHOR", "Åsmund Stavdahl");
 Environment::set("DESCRIPTION", "Hold oversikt over Bygger'n-utstyr");
 
